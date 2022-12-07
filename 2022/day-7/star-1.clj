7beb85733b9f7b6f8e17baca02d7629f563d4a43
nadir-noom <newmania.9@gmail.com>
2022-12-07 22:40:18 +0000

    Add solution for day-7 of advent of code
    This commit adds the star-1 and star-2 solutions for advent of code. We
    first build up a a nested hash map of files/directories, and then we
    walk the file system to calculate the totals of each file system.

---
+++ 2022/day-7/star-1.clj
@@ -0,0 +1,45 @@
+#!/usr/bin/env bb
+(require '[clojure.set :as set])
+(require '[clojure.walk :as w])
+
+(defn clean-up [node]
+  (if (vector? node)
+    (if (map? (second node))
+      (when (<= (:total (second node)) 100000)
+        (println (:total (second node))))))
+  node)
+
+(defn df [node]
+  (if (vector? node)
+    (if (map? (second node))
+      (let [dir (second node)
+            size (apply + (filter #(not (map? %)) (vals dir)))
+            child-size (apply + (map :total (filter map? (vals dir))))]
+       [(first node) (assoc-in dir [:total] (+ child-size size))])
+      node)
+    node))
+
+(defn fs-tree [commands]
+  (loop [commands commands
+         fs {}
+         path []]
+    (let [command (str/split (first commands) #" ")]
+      (cond
+        (and (= (nth command 0) "$") (= (nth command 1) "cd"))
+          (if (= (nth command 2) "..")
+            (recur (drop 1 commands) fs (vec (drop-last path)))
+            (recur (drop 1 commands) fs (conj path (nth command 2))))
+        (or (= (nth command 0) "dir") (and (= (nth command 0) "$") (= (nth command 1) "ls")))
+          (recur (drop 1 commands) fs path)
+        :default
+          (let [updated-fs (assoc-in fs (conj path (nth command 1)) (Integer/parseInt (nth command 0)))]
+            (if (> (count commands) 1)
+              (recur (drop 1 commands) updated-fs path)
+              updated-fs))))))
+
+(->> (slurp "data.txt")
+     (str/split-lines)
+     (fs-tree)
+     (w/postwalk df)
+     (w/postwalk clean-up)
+     (pprint))
