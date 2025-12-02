f90b0a8398ffc1436202b2dd3328f63ff9385e91
nadir-noom <newmania.9@gmail.com>
2025-12-02 00:23:54 +0000

    Add star-1 solution for day-1

--- 2025/day-1/star-1.clj
+++ 2025/day-1/star-1.clj
@@ -2,4 +2,26 @@
 
 (require '[clojure.string :as str])
 
-(println "Test")
+(def directions {"L" - "R" +})
+
+(defn position [current step]
+  (let [direction (get directions (first step))
+        new-position (direction current (second step))]
+    (mod new-position 100)))
+
+(defn turn-tumbler [tumbler step]
+  (let [new-position (position (:position tumbler) step)]
+    {:position new-position
+     :password (if (= new-position 0) 
+                 (inc (:password tumbler)) 
+                 (:password tumbler))}))
+
+(defn parse-line [line]
+  (let [[direction & parts] (str/split line #"")]
+    [direction (Integer/parseInt (apply str parts))]))
+
+(->> (slurp "data.txt")
+     (str/split-lines)
+     (map parse-line)
+     (reduce turn-tumbler {:position 50 :password 0})
+     (println))
